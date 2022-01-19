788488c08c5c90c812877d81a62cdeca5e6afc43
AndreyBunesh <senior_21@mail.ru>
2022-01-19 18:29:02 +0300

    Add project files.

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+
+        {
+            var name = Console.ReadLine();
+            string AdminName = Environment.UserName;
+          string currentDate = DateTime.Now.ToShortDateString();
+            Console.OutputEncoding = Encoding.UTF8;
+
+            Console.WriteLine("Привет "+ AdminName + " сегодня " + currentDate);
+
+            
+                        Console.WriteLine(name);
+
+        }
+    }
+}
